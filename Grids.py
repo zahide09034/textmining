40f2e7f5d008916df58195cce6b7fc3bc06762d8
zahide09034 <zahideceliksu@gmail.com>
2022-10-21 15:46:28 +0300

    Colaboratory kullanılarak oluşturuldu

---
+++ Grids.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPcACN8PrBE8yv3iQ5tIHWb",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/zahide09034/textmining/blob/master/Grids.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "-Hd71FEuk1yX"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
